f14029cc297a43c7f5faa954f483b54150a8f048
박민호 <radlohead@naver.com>
2019-01-12 09:34:23 +0900

    .

---
+++ src/components/ProductTab.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import './ProductTab.scss';
+
+const ProductTab = () => {
+    return (
+        <div className="ProductTab">test</div>
+    )
+}
+
+export default ProductTab;
---
+++ src/components/RankingTab.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import './RankingTab.scss';
+
+const RankingTab = () => {
+    return (
+        <div className="RankingTab">test2</div>
+    )
+}
+
+export default RankingTab;
