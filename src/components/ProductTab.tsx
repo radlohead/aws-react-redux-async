7f1a5328195157a41b41eb9d10fb030a760fbfe9
박민호 <radlohead@naver.com>
2019-01-13 12:50:18 +0900

    product css 적용

--- src/components/ProductTab.tsx
+++ src/components/ProductTab.tsx
@@ -4,14 +4,23 @@ import './ProductTab.scss';
 const ProductTab = (productTabItemsJSON: any) => {
     return (
         <div className='product__tab'>
-            <ul className="product__list">
-                {productTabItemsJSON.json.map((v: any, i: any) => {
-                    return <li key={i}>
-                        <span className="">{v.price}</span>
-                        <img src={v.image.url} alt={v.title} />
-                    </li>;
+            {productTabItemsJSON.json.map((v: any, i: any) => { 
+                return <ul key={i} className="product__tab__list">
+                    {[0, 1, 2].map((index: any) => {
+                        return <li key={index} className="product__tab__list__item">
+                            {(i+3) % 3 !== 0 || index !== 0
+                                || <span className="product__tab__list__item__description">
+                                        <em className="product__tab__list__item__description__shop-name">{v.shopName}</em>
+                                        <em className="product__tab__list__item__description__title">{v.title}</em>
+                                    </span>}
+                            {(i+3) % 3 !== 0 || index !== 0
+                                && <span className="product__tab__list__item__price">{v.price}</span>}
+                            {(i+3) % 3 !== 0
+                                && <span className="product__tab__list__item__price">{v.price}</span>}
+                            <img className="product__tab__list__item__img" src={v.image.url} alt={v.title} />
+                        </li>;
                 })}
-            </ul>
+            </ul>})}
         </div>
     )
 }
