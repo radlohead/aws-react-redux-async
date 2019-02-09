7af513bbda237abf2db782c4b8e6bc3799d2ba77
박민호 <radlohead@naver.com>
2019-02-09 12:59:46 +0900

    redux-dev-tools 적용

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from './containers/App';
 import reducer from './reducers';
@@ -11,7 +12,7 @@ import './css/index.scss';
 
 const store = createStore(
 	reducer, 
-	applyMiddleware(thunk, logger)
+	composeWithDevTools(applyMiddleware(thunk, logger))
 );
 
 ReactDOM.render(
